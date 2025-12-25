4779fe6e4665b316249a0e631c93427cee9a023b
Casey McGrath <caseymcgrath77@yahoo.com>
2025-12-24 19:15:00 -0500

    Initialize sphinx documentation, using the 'Sphinx Book Theme'.

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'The MCMC Cookbook'
+copyright = '2025, Casey McGrath'
+author = 'Casey McGrath'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_title = "The MCMC Cookbook"
+
+
+html_theme_options = {
+    "repository_url": "https://github.com/CaseyMcGrath/The-MCMC-Cookbook",
+    "use_repository_button": True,
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,19 @@
+.. The MCMC Cookbook documentation master file, created by
+   sphinx-quickstart on Tue Dec 23 21:45:24 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+A Recipe Guidebook for Crafting MCMCs
+=====================================
+
+
+
+
+
+
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
