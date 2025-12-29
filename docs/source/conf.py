9813b8f1321db18847eaa817ebc4d810343e4a06
Casey McGrath <caseymcgrath77@yahoo.com>
2025-12-29 01:25:35 -0500

    Revert: remove the jupyter notebook compatibility
    I realized that I probably don't wwant to compile the jupyter notebooks when I run 'make html' for creating the pages on this website.  So I am commenting out the 'myst_nb' and re-instating the 'myst_parser' in the conf.py file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,12 @@ author = 'Casey McGrath'
 
 # !!EXTENSIONS Note!!
 # --> myst_nb supercedes myst_parser when it is used, and myst_parser should be commented out!
-extensions = [#"myst_parser",      # https://myst-parser.readthedocs.io/en/latest/intro.html
-"sphinxcontrib.bibtex",           # https://sphinxcontrib-bibtex.readthedocs.io/en/latest/quickstart.html
-"sphinx_design",                  # https://myst-parser.readthedocs.io/en/latest/intro.html#extending-sphinx
-"myst_nb",                        # https://myst-nb.readthedocs.io/en/latest/quickstart.html 
-"sphinx_copybutton",              # https://sphinx-copybutton.readthedocs.io/en/latest/
+extensions = [
+    "myst_parser",            # https://myst-parser.readthedocs.io/en/latest/intro.html
+    "sphinxcontrib.bibtex",   # https://sphinxcontrib-bibtex.readthedocs.io/en/latest/quickstart.html
+    "sphinx_design",          # https://myst-parser.readthedocs.io/en/latest/intro.html#extending-sphinx
+    #"myst_nb",                # https://myst-nb.readthedocs.io/en/latest/quickstart.html 
+    "sphinx_copybutton",      # https://sphinx-copybutton.readthedocs.io/en/latest/
 ]
 
 templates_path = ['_templates']
